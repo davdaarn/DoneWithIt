07e8bfacf31b62a9f1b61286edca550a9364ca34
David Long <davdaarn@live.com>
2022-06-06 08:03:46 -0600

    Testing Screens

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { ViewImageScreen } from './app/screens/ViewImageScreen';
 import { WelcomeScreen } from './app/screens/WelcomeScreen';
-
+import AppCard from './app/components/AppCard';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 export default function App() {
   return (
-      // <WelcomeScreen />
-      <ViewImageScreen />
+    // <WelcomeScreen />
+    <ViewImageScreen />
+    // <ListingDetailsScreen />
   );
 }
 
